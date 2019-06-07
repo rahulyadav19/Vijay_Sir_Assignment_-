66f08fa47b7f10907fca0b19f7a36cbac4b8a4bc
rahulyadav19 <34089130+rahulyadav19@users.noreply.github.com>
2019-06-08 00:50:48 +0530

    Create test1_file.py

---
+++ test1_file.py
@@ -0,0 +1,18 @@
+
+num_of_values= input("Enter number of key-value pairs")
+type(num_of_values)
+
+tot= num_of_values*2
+i=0
+for i in tot:
+    print (i)
+    keys=input("Enter Key")
+    values=input("Enter value")
+    i=i+1
+
+
+for line in list:
+    id = line[0]
+    values = line[1]
+
+# This didn't work. It says not iterable
